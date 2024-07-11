6e7f7a4d4d07f1e40cb9f4ef6a30465055e0b8fd
Masood-Haider <masood.haider.bangash1@gmail.com>
2024-07-12 00:37:08 +0500

    h

---
+++ script.js
@@ -0,0 +1,24 @@
+// let a,b;
+// a=prompt("enter string",);
+// b=prompt("enter index",);
+// let c=a.charAt(b);
+// console.log(c);
+
+// let a=prompt("enter a long string",);
+// let b=prompt("enter a substring to replace",);
+// let c=prompt("enter the replacement");
+// let d=a.replace(b,c);
+// console.log(d);
+
+
+// let a=prompt("enter a number that will be then rounded",);
+// let b=Math.round(a);
+// console.log(b);
+
+// let a=prompt("enter a number that will be then rounded to its upper value",);
+// let b=Math.ceil(a);
+// console.log(b);
+
+// let c=prompt("enter a number that will be then rounded to its lower value",);
+// let d=Math.floor(c);
+// console.log(d);
